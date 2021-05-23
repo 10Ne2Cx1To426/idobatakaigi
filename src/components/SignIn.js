088a9ab92394562ebf1798847f62f894bf613da9
Sena <senapon.214@gmail.com>
2021-05-23 11:32:56 +0900

    save name in japanese

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -43,7 +43,7 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [ disabled, setDisabled ] = useState(true);
   const [ string, setString ] = useState('');
-  console.log({string});
+  const [ isComposed, setIsComposed ] = useState(false);
 
   useEffect(() => {
     const disabled = string === ''
@@ -69,12 +69,14 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown={(e) => {
-              console.log({ key: e.key})
+              if  (isComposed) return;
               if( e.key === 'Enter') {
                 setName(e.target.value);
                 e.preventDefault();
               }
-              }}
+            }}
+            onCompositionStart={() => setIsComposed(true)}
+            onCompositionEnd={() => setIsComposed(false)}
           />
           <Button
             type="button"
