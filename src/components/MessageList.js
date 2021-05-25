0643c407e12d815d5d2803491ab5d2e577eea21d
Sena <senapon.214@gmail.com>
2021-05-25 17:02:08 +0900

    display messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles'
+import { List } from '@material-ui/core';
 
-import { messagesRef } from '../firebase'
+import { messagesRef } from '../firebase';
+import MessageItem from './MessageItem'
 
 const useStyles = makeStyles({
   root: {
-    gridRow: 1
+    gridRow: 1,
+    overflow: 'auto',
+    width: '100%'
   }
 })
 
 const MessageList = () => {
-  const [ messages, setMessages ] = useState('');
+  const [ messages, setMessages ] = useState([]);
   const classes = useStyles();
 
   useEffect(() => {
     messagesRef
     .orderByKey()
-    .limitToLast(3)
+    .limitToLast(15)
     .on('value', (snapshot) => {
       const messages = snapshot.val();
       if ( messages === null) return;
@@ -25,11 +29,16 @@ const MessageList = () => {
         const [ key, nameAndText] = entry;
         return { key, ...nameAndText };
       });
-      console.log(newMessages);
       setMessages(newMessages);
   });
-  }, [])
-  return <div className={classes.root}>MessageList</div>
+  }, []);
+  return (
+    <List className={classes.root}>
+      {messages.map(({key, name, text}) => {
+        return <MessageItem key={key} name={name} text={text}></MessageItem>
+      })}
+    </List>
+  );
 };
 
 export default MessageList;
